4a37aea25078f44a738c3f6a5cfaa7ff00538702
papri221003 <bhpapri03@gmail.com>
2026-03-14 10:50:46 +0530

    initial commit

---
+++ List_Tuples.py
@@ -0,0 +1,37 @@
+#WAP to the user to give the movie names and store them in the list
+"""
+movie=[]
+movie.append(input("Enter the movie name: "))
+movie.append(input("Enter the movie name: "))
+movie.append(input("Enter the movie name: "))
+
+print(movie)
+
+"""
+
+#WAP to check if the list contains the elements they are pallindrome or not
+
+list=[1,2,2,1]
+
+list2=list.copy()
+list2.reverse()
+
+if(list==list2):
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+    
+
+#WAP to count the number of students of grade A
+
+student=("A","A","C","B","D","B")
+print("'A' Graded students are: ",student.count("A"))
+print("'B' Graded students are: ",student.count("B"))
+print("'C' Graded students are: ",student.count("C"))
+
+List_Student=["A","A","C","B","D","B"]
+List_Student.sort()
+print(List_Student)
+
+
+#started git hub.....
---
+++ first_prog.py
@@ -0,0 +1,30 @@
+#print("Hello World")
+
+#WAP to take two input and add them and give the sum result
+"""
+num1=int(input("Enter the First number: "))
+num2=int(input("Enter the Second number: "))
+
+print("The sum result is: ",num1+num2)
+
+"""
+
+#WAP to take a input of one side of the square and print its area
+
+num1=int(input("Enter the side of the square: "))
+print("The area of the square is: ",num1*num1)
+
+#WAP tp input two integer numbers a and b.Print True if a is greater than or equal to b if not print false
+a=int(input("Enter the First number: "))
+b=int(input("Enter the Second number: "))
+
+if (a>b):
+    print("True")
+elif (a<b):
+    print("Lesser")
+else:
+    print("Equal")
+
+
+
+
