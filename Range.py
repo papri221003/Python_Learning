76bd6d2edc2866be30d25226c9b6667ed65d7959
papri221003 <bhpapri03@gmail.com>
2026-03-14 21:13:03 +0530

    initial commit

---
+++ Range.py
@@ -0,0 +1,12 @@
+seq=range(101)
+for idx in seq:
+    print(idx)
+    
+seq1=range(100,0,-1)
+for i in seq1:
+    print(i)
+    
+seq3=range(11)
+n=int(input("Enter the number: "))
+for j in seq3:
+    print(n*j)
