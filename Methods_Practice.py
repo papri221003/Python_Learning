a95c97f9bd47f9d53f9ae0f6fef840d3ef195c4b
papri221003 <bhpapri03@gmail.com>
2026-03-18 11:47:13 +0530

    initial commit

--- Methods_Practice.py
+++ Methods_Practice.py
@@ -1,5 +1,8 @@
 class Student:
     clg_name="ABC COLLEGE"
+    @staticmethod
+    def greet():
+        print("Welcome! in the new college")
     
     def __init__(self,name,marks):
         self.name=name
@@ -12,8 +15,11 @@ class Student:
             sum+=self.marks[i]
             i+=1
         
-        print("Hi!!",self.name,"your avg score is: ",sum/3)    
+        print("Hi!!",self.name,"your avg score is: ",sum/3) 
+    
+       
         
 s1=Student("Trisha",[95,96,97])
+s1.greet()
 
 s1.get_avg()
