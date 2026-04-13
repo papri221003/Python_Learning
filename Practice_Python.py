f3a2652398e180ac12ed280af7b5106d234b702d
papri221003 <bhpapri03@gmail.com>
2026-04-13 10:24:17 +0530

    initial commit

--- Practice_Python.py
+++ Practice_Python.py
@@ -1,7 +1,13 @@
+
 name=input("What is your name?")
 age=input("What is his age?")
 hobby=input("What is your hobby?")
-print("My name is "+name+" and I'm "+age+" years old.My favourite hobby is "+hobby)
+print("My name is ",name," and I'm ",age," years old.My favourite hobby is ",hobby)
 superhero=input("What is your SuperHero name Miss."+name+"?")
-print("My superhero name is: "+superhero)
+print("My superhero name is: ",superhero)
 
+
+old_age=int(input("Enter the age: "))
+print(type(old_age))
+new_Age=old_age+2
+print("New Age is: ",new_Age)
