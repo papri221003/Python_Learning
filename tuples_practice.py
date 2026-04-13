2d499abf7eec98204fa79886f5b610b77915736b
papri221003 <bhpapri03@gmail.com>
2026-04-13 19:05:10 +0530

    initial commit

---
+++ tuples_practice.py
@@ -0,0 +1,17 @@
+#Tupple
+"""
+tup=(97,98,95,96,93,91,92,94,97,97)
+# tuples are immutable...means creating adding or removing any elemt from a tuple is not possible s it is immutable..once it is created it cant be deleted or rectified
+#tup[0]=14 .........not allowed
+print(tup.count(97))
+print(tup.index(98))
+"""
+
+#Set 
+nums={1,2,4,5,8,1,2,6,9,7,4,2}
+print(nums)
+nums.add(10)
+print(nums)
+nums1={10,15,18,17,19}
+print(nums.union(nums1))
+print(nums.intersection(nums1))
