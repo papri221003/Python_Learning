1106f0b0a4311685396c6384346521e309f39d04
papri221003 <bhpapri03@gmail.com>
2026-04-13 10:09:17 +0530

    initial commit

---
+++ Practice_Python.py
@@ -0,0 +1,7 @@
+name=input("What is your name?")
+age=input("What is his age?")
+hobby=input("What is your hobby?")
+print("My name is "+name+" and I'm "+age+" years old.My favourite hobby is "+hobby)
+superhero=input("What is your SuperHero name Miss."+name+"?")
+print("My superhero name is: "+superhero)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+int(input("Enter the number: "))
