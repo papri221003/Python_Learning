76710c4058d68a1d7d03c93f80a3c62e85dd95dd
papri221003 <bhpapri03@gmail.com>
2026-04-13 10:40:10 +0530

    initial commit

---
+++ Srting_Operations.py
@@ -0,0 +1,10 @@
+Str="My name is Papri Bhattacharjee"
+print(len(Str))
+print(Str[1:15]) #slicing string[starting index:endingindex+1]
+print(Str.upper())#all upper case convert kore print kore but main string aki thakbe
+print(Str.lower())#all lower case convert kore print kore but main string aki thakbe
+print(Str.endswith("ee")) #"ee" diye sesh hole true hobe or else false hobe
+print(Str.replace("Papri","Teesta"))
+print(Str.find('a')) #word tar index debe..word ta ekbarer besi thakle 1st finding index debe
+print(Str.count('a'))
+print('Papri' in Str)
