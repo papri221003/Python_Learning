9ea9a7f6ca8b6ec79aa0e4bd8f89b644129d7ed2
papri221003 <bhpapri03@gmail.com>
2026-09-07 11:51:02 +0530

    sept try

---
+++ chapter1.py
@@ -0,0 +1,27 @@
+"""
+#Adding two varible taken by user
+num1=int(input("Enter the number 1: "))
+num2=int(input("Enter the number 2: "))
+
+print("The answer of the operation is: ",num1+num2)
+
+
+#Printing its Area
+side=int(input("Enter the value of the side of a square: "))
+print("It's area is: ",side*side)
+
+#Take the input of two floating numbers and print its avg
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+ans=(num1+num2)/2
+print("The avg of two number: ",ans)
+"""
+
+#Take two numbers and check whether it is larger than one another or not
+a=int(input("Enter the first value: "))
+b=int(input("Enter the second value: "))
+
+if(a>=b):
+    print("True")
+else:
+    print("False")
--- tuples_practice.py
+++ tuples_practice.py
@@ -8,10 +8,26 @@ print(tup.index(98))
 """
 
 #Set 
-nums={1,2,4,5,8,1,2,6,9,7,4,2}
+"""
+nums={1,2,4,5,8,1,2,6,9,7,4,2,(40,85,'papri')}
 print(nums)
 nums.add(10)
 print(nums)
 nums1={10,15,18,17,19}
 print(nums.union(nums1))
 print(nums.intersection(nums1))
+"""
+
+#Dictionary
+my_Dict={}
+
+my_Dict.update({"Name":"Papri"})
+my_Dict.update({"cgpa":8.72})
+my_Dict.update({"Subjects":('Control','Analog System','Device','Signal system')})
+my_Dict.update({"Marks":(85,91,96,87)})
+
+print(my_Dict)
+print(my_Dict["Marks"])
+print(my_Dict.keys())
+print(my_Dict.values())
+
